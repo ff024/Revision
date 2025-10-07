d286a7a456e25df0d9f2d8b9f1ccc599ceb69755
ff024 <fabrice.fasel@gmail.com>
2025-10-07 19:02:12 +0200

    ajout

---
+++ db/init/02-seed.sql
@@ -0,0 +1,6 @@
+USE voc_db;
+
+INSERT INTO word (original_word, translation) VALUES
+  ('bonjour', 'hello'),
+  ('maison',  'house'),
+  ('chien',   'dog');
--- db/seed.sql
+++
@@ -1,8 +0,0 @@
-USE voc_db;
-
-INSERT INTO
-    word (original_word, traduction)
-VALUES
-    ('bonjour', 'hello'),
-    ('maison', 'house'),
-    ('chien', 'dog');
